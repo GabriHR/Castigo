b2f58aff6d41e5e704ebe0c9d4e60f168e3e8449
GabriHR <ghernrod@myuax.com>
2024-02-07 14:33:38 +0100

    Bucle while que abarca todos los numeros enteros que sean <10. Bucle en el que se si el numero no es primo, el bucle se rompe. Si el numero es primo lo printea.

--- Ejercicio6.java
+++ Ejercicio6.java
@@ -2,5 +2,22 @@ public class Ejercicio6 {
     public static void main(String[] args) {
         int count = 0;
         int num = 2;
+
+        while (count < 10) {
+            boolean esPrimo = true;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+
+            if (esPrimo) {
+                System.out.println(num);
+                count++;
+            }
+
+            num++;
+        }
     }
 }
