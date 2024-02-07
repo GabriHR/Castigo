285ad5c011de03acef0dc06d4476113bed20d51d
GabriHR <ghernrod@myuax.com>
2024-02-07 14:35:47 +0100

    Metodo void en el que se definen las variables requeridas y le pide los valores al usuario.

---
+++ Ejercicio7.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un número entero:");
+        int num = scanner.nextInt();
+        int originalNum = num;
+    }
+}
