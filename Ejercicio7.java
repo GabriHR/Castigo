123a3d686faef53028d8160a92524a648a4b9bf9
GabriHR <ghernrod@myuax.com>
2024-02-07 14:38:04 +0100

    Printeo de los numeros en los que se factoriza el valor insertado por el usuario.

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -6,5 +6,13 @@ public class Ejercicio7 {
         System.out.println("Introduce un número entero:");
         int num = scanner.nextInt();
         int originalNum = num;
+
+        System.out.print("Los factores primos de " + originalNum + " son: ");
+        for (int i = 2; i <= num; i++) {
+            while (num % i == 0) {
+                System.out.print(i + " ");
+                num /= i;
+            }
+        }
     }
 }
