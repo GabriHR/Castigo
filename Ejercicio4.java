70ad5b6fe9204d2d786f16972dc58ab3c3127b35
GabriHR <ghernrod@myuax.com>
2024-02-07 14:24:34 +0100

    Bucle while para abarcar todos los casos posibles. El bucle se rompe cuando el usuario entrega un numero negativo.

--- Ejercicio4.java
+++ Ejercicio4.java
@@ -5,5 +5,22 @@ public class Ejercicio4 {
         Scanner scanner = new Scanner(System.in);
         int max = Integer.MIN_VALUE;
         int min = Integer.MAX_VALUE;
+
+        while (true) {
+            System.out.println("Introduce un numero entero positivo (o un numero negativo para terminar):");
+            int num = scanner.nextInt();
+
+            if (num < 0) {
+                break;
+            }
+
+            if (num > max) {
+                max = num;
+            }
+
+            if (num < min) {
+                min = num;
+            }
+        }
     }
 }
