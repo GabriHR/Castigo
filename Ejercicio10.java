8708723470626a0b2a92740bf94678914799a628
GabriHR <ghernrod@myuax.com>
2024-02-07 14:52:15 +0100

    Bucle for que recorre cada fila de la matriz con un bucle interno que recorre cada columna de la matriz apartir de la diagonal (donde j=i).

--- Ejercicio10.java
+++ Ejercicio10.java
@@ -8,5 +8,15 @@ public class Ejercicio10 {
         System.out.println("Introduce la dimension de la matriz:");
         int dimension = scanner.nextInt();
         int[][] matriz = new int[dimension][dimension];
+
+        for (int i = 0; i < dimension; i++) {
+            for (int j = i; j < dimension; j++) {
+                int num = random.nextInt(100);
+                matriz[i][j] = num;
+                matriz[j][i] = num;
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
