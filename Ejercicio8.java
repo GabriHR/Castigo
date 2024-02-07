469c137471289528e65eca6c07ffeb125fe94207
GabriHR <ghernrod@myuax.com>
2024-02-07 14:41:29 +0100

    Bucle do en el que se pide al usuario que introducza numeros enteros y el programa calcula los numeros maximos y minimos ya definididos anteriormente.

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -5,5 +5,16 @@ public class Ejercicio8 {
         Scanner scanner = new Scanner(System.in);
         int num;
         int sum = 0, count = 0, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+
+        do {
+            System.out.println("Introduce un numero entero (0 para terminar):");
+            num = scanner.nextInt();
+            if (num != 0) {
+                sum += num;
+                count++;
+                if (num < min) min = num;
+                if (num > max) max = num;
+            }
+        } while (num != 0);
     }
 }
