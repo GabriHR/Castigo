7ce0226088ce81dcaec325fb42045d9f90094966
GabriHR <ghernrod@myuax.com>
2024-02-07 14:39:45 +0100

    Metodo void main en el que se definen las variables requeridas.

---
+++ Ejercicio8.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        int sum = 0, count = 0, min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
+    }
+}
